d9bbf470905a7ae8378a257ca9b0f57b8e121818
chandrakumarreddy <chandrapenugonda655@gmail.com>
2021-03-19 23:02:50 +0530

    get task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Task } from './Task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  @Get(':id')
+  getById(@Param('id', ParseIntPipe) taskId: number): Promise<Task> {
+    return this.tasksService.getById(taskId);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './Task.entity';
+import { TaskRepository } from './Task.repository';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private taskRepository: TaskRepository
+  ) {}
+  async getById(taskId: number) {
+    const task = await this.taskRepository.findOne(taskId);
+    if (!task) {
+      throw new NotFoundException(`TaskId not found ${taskId}`);
+    }
+    return task;
+  }
+}
