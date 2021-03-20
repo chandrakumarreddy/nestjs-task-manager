3ce1061fd4586bdacdeb45dd182a0140e0f37bba
chandrakumarreddy <chandrapenugonda655@gmail.com>
2021-03-20 23:08:17 +0530

    create auth module,service,controller,entity and repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeormConfig from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig.getTypeOrmConfig()), TasksModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig.getTypeOrmConfig()), TasksModule, AuthModule],
   controllers: [],
   providers: []
 })
