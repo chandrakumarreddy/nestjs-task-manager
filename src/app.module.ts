db1cbc692cb7d8e77b8abae24a84f30df772c2d5
chandrakumarreddy <chandrapenugonda655@gmail.com>
2021-03-22 00:10:59 +0530

    setup passport stategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import typeormConfig from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig.getTypeOrmConfig()), TasksModule, AuthModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: 3600 } }),
+    TypeOrmModule.forRoot(typeormConfig.getTypeOrmConfig()),
+    TasksModule,
+    AuthModule
+  ],
   controllers: [],
   providers: []
 })
